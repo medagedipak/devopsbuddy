950e1e5a5a0f39bcce7f8d70ce1d126865f33812
Dipak Medage <medagedipak@gmail.com>
2022-05-26 23:30:02 +0530

    Added test for i18n service

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,13 +1,28 @@
 package com.devopsbuddy;
 
+import com.devopsbuddy.web.i18n.I18NService;
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DevopsbuddyApplicationTests {
 
+	@Autowired
+	private I18NService i18NService;
+
+
 	@Test
-	void contextLoads() {
+	public void testMessageByLocalService() throws Exception{
+		String expectedResult="Bootstrap starter templates";
+		String messageId="index.main.callout";
+		String actual=i18NService.getMessage(messageId);
+		Assert.assertEquals("The actual and expected Strings don't match", expectedResult, actual);
+
 	}
+
 
 }
